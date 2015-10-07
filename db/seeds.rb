177ce6b3a4030a57f85ac77eea280df0fe96ced2
Haile Xiao <haile.xiao@gmail.com>
2015-10-06 20:10:33 -0400

    feed the hound 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Neighborhood.create(
-    [{ name: 'Chinatown' }, { name: 'Back Bay' }, { name: 'Financial District' },
+    [
+    { name: 'Chinatown' }, { name: 'Back Bay' }, { name: 'Financial District' },
     { name: 'North End' }, { name: 'South End' }, { name: 'West End' },
     { name: 'South Boston' }, { name: 'East Boston' }, { name: 'Charlestown' },
     { name: 'Allston' }, { name: 'Brighton' }, { name: 'Fenway' },
     { name: 'Jamaica Plain' }, { name: 'Roxbury' }, { name: 'Dorchester' },
     { name: 'Cambridge' }, { name: 'Somerville' }
-    ])
+    ]
+    )
 
 GasUtility.create([{ name: 'National Grid' }, { name: 'Eversource' }])
 
